cd6951bdf1e3059c18c65bc7f0157903ec5503d6
Pnic0las <78710449+Pnic0las@users.noreply.github.com>
2021-10-16 11:41:15 +0200

    Use Effect In Card Game Module

--- src/Views/InGame.js
+++ src/Views/InGame.js
@@ -3,15 +3,11 @@ import '../InGame.css';
 import cards from '../Cards';
 
 export default function Board() {
-  const Deck = { cards };
-
   // const GameStatus = {
   //   NOTSTARTED: 0,
   //   FINISHED: 1,
   //   INPROGRESS: 2,
   // };
-
-  const textwinner = '';
 
   function shuffle() {
     for (let index = cards.length - 1; index > 0; index -= 1) {
@@ -23,23 +19,40 @@ export default function Board() {
     return cards;
   }
 
-  Deck.cards = shuffle();
-  const deckMidpoint = Math.ceil(Deck.cards.length / 2);
-  const playerDeck = Deck.cards.slice(0, deckMidpoint);
-  const computerDeck = Deck.cards.slice(deckMidpoint, Deck.cards.length);
+  const textwinner = '';
+  const [Deck] = React.useState(shuffle());
+  const [playerADeck, setplayerADeck] = React.useState([]);
+  const [PlayerBDeck, setplayerBDeck] = React.useState([]);
 
-  console.log('Payer Deck ==>', playerDeck);
-  console.log('Computer Deck ==>', computerDeck);
-  console.log(Deck);
+  React.useEffect(() => {
+    const deckMidpoint = Math.ceil(Deck.length / 2);
+    setplayerADeck(Deck.slice(0, deckMidpoint));
+    setplayerBDeck(Deck.slice(deckMidpoint, Deck.length));
+  }, [Deck]);
+
+  React.useEffect(() => {
+    console.log('Player A Black =>', playerADeck);
+    console.log('Player A White =>', PlayerBDeck);
+  }, [playerADeck, PlayerBDeck]);
 
   return (
     <div className="board">
       <div className="computer-deck deck">
-        {/* <img src={Deck.cards[0].assets} alt="ImageCard" /> */}
+        <img
+          className="card"
+          src={playerADeck[0]?.assets_black}
+          alt="ImageCard"
+        />
       </div>
       <div className="computer-card-slot card-slot" />
       <div className="text">{textwinner}</div>
-      <div className="player-deck deck" />
+      <div className="player-deck deck">
+        <img
+          className="card"
+          src={PlayerBDeck[0]?.assets_white}
+          alt="ImageCard"
+        />
+      </div>
       <div className="player-card-slot card-slot" />
     </div>
   );
