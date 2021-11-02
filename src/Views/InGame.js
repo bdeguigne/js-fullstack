4fd4e0a0237e9a2ebf41566f10d220e8c4d14bd5
Pnic0las <78710449+Pnic0las@users.noreply.github.com>
2021-11-02 18:03:34 +0100

    Card Game presque fini

--- src/Views/InGame.js
+++ src/Views/InGame.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import '../InGame.css';
+import cardBack from '../card-back.png';
 import cards from '../Cards';
 
 export default function Board() {
-  // const GameStatus = {
-  //   NOTSTARTED: 0,
-  //   FINISHED: 1,
-  //   INPROGRESS: 2,
-  // };
-
   function shuffle() {
     for (let index = cards.length - 1; index > 0; index -= 1) {
       const NewIndex = Math.floor(Math.random() * (index + 1));
@@ -19,10 +14,57 @@ export default function Board() {
     return cards;
   }
 
-  const textwinner = '';
   const [Deck] = React.useState(shuffle());
   const [playerADeck, setplayerADeck] = React.useState([]);
-  const [PlayerBDeck, setplayerBDeck] = React.useState([]);
+  const [playerBDeck, setplayerBDeck] = React.useState([]);
+  const [playerACard, setplayerACard] = React.useState([]);
+  const [playerBCard, setplayerBCard] = React.useState([]);
+  const [textwinner, settextwinner] = React.useState(
+    'Press Start Round to begin',
+  );
+  const [gameStatus, setGameStatus] = React.useState('NOTSTARTED');
+
+  function handleStartClick() {
+    setGameStatus('INPROGRESS');
+  }
+
+  React.useEffect(() => {
+    if (playerACard.value !== undefined && playerBCard.value !== undefined) {
+      console.log(
+        'player B value =>',
+        playerBCard.value,
+        'player A value =>',
+        playerACard.value,
+      );
+
+      if (playerACard.value > playerBCard.value) {
+        settextwinner('Winner is Pseudo player A');
+      } else {
+        settextwinner('Winner is Pseudo player B');
+      }
+    }
+  }, [playerACard, playerBCard]);
+
+  function handleRoundClick() {
+    setplayerACard(playerADeck.pop());
+    setplayerBCard(playerBDeck.pop());
+  }
+
+  function StartGameButton() {
+    return (
+      <button type="button" onClick={handleStartClick}>
+        START GAME
+      </button>
+    );
+  }
+
+  function StartRoundButton() {
+    return (
+      <button type="button" onClick={handleRoundClick}>
+        START ROUND
+      </button>
+    );
+  }
 
   React.useEffect(() => {
     const deckMidpoint = Math.ceil(Deck.length / 2);
@@ -32,28 +74,49 @@ export default function Board() {
 
   React.useEffect(() => {
     console.log('Player A Black =>', playerADeck);
-    console.log('Player A White =>', PlayerBDeck);
-  }, [playerADeck, PlayerBDeck]);
+    console.log('Player B White =>', playerBDeck);
+  }, [playerADeck, playerBDeck]);
+
+  React.useEffect(() => {
+    console.log('Player A Black Card  =>', playerACard);
+    console.log('Player B White Card =>', playerBCard);
+  }, [playerACard, playerBCard]);
+
+  React.useEffect(() => {
+    console.log('Game Status =>', gameStatus);
+  }, [gameStatus]);
 
   return (
     <div className="board">
-      <div className="computer-deck deck">
-        <img
-          className="card"
-          src={playerADeck[0]?.assets_black}
-          alt="ImageCard"
-        />
-      </div>
-      <div className="computer-card-slot card-slot" />
-      <div className="text">{textwinner}</div>
-      <div className="player-deck deck">
-        <img
-          className="card"
-          src={PlayerBDeck[0]?.assets_white}
-          alt="ImageCard"
-        />
-      </div>
-      <div className="player-card-slot card-slot" />
+      {gameStatus === 'INPROGRESS' && (
+        <>
+          <div className="computer-deck deck">
+            <img className="card" src={cardBack} alt="ImageCardBack" />
+          </div>
+          <div className="computer-card-slot card-slot">
+            {playerACard !== undefined && (
+              <img className="card" src={playerACard.assets_black} alt="" />
+            )}
+          </div>
+          <div className="text">{textwinner}</div>
+          <div>
+            <StartRoundButton />
+          </div>
+          <div className="player-deck deck">
+            <img className="card" src={cardBack} alt="ImageCardBack" />
+          </div>
+          <div className="player-card-slot card-slot">
+            {playerBCard !== undefined && (
+              <img className="card" src={playerBCard.assets_white} alt="" />
+            )}
+          </div>
+        </>
+      )}
+      {gameStatus === 'NOTSTARTED' && (
+        <div>
+          <StartGameButton />
+        </div>
+      )}
     </div>
   );
 }
