07d1dcc5f0aa04fef8b1c706436e2f866e5566ee
kynail <kylian.gence@epitech.eu>
2021-11-07 18:02:49 +0400

    correct conflit git

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,14 +3,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import { logUser, registerUser } from '../redux/action/LoginAction';
 import Forms from '../components/Forms';
+import { registerUser, logUser } from '../redux/action/LoginAction';
 import CardAnimation from '../components/AnimationHome';
 import '../utils/i18n.js';
 
 function Home(props) {
   const { t, i18n } = useTranslation();
-  // const {langue} = props;
   const changeLanguage = (event) => {
     i18n.changeLanguage(event.target.value);
   };
@@ -20,7 +19,6 @@ function Home(props) {
     <div className="Container" onChange={changeLanguage}>
       <h1> {t('Welcome.label')} </h1>
       <CardAnimation />
-      <Forms onSubmit={(Password, Pseudo) => props.logUser(Pseudo, Password)} />
       <Forms
         onSubmit={async (Password, Pseudo) => {
           const bool = await props.logUser(Pseudo, Password);
